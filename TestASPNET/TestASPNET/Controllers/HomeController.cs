7d64a82ab025ce3a30b62b87d07001279bf75a26
Ludwig Nassen <35228327+ludwignassen@users.noreply.github.com>
2023-01-19 11:19:33 +0100

    Test

---
+++ TestASPNET/TestASPNET/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TestASPNET.Models;
+
+namespace TestASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
---
+++ TestASPNET/TestASPNET/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddMvc(
+    options => options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+        (v, p) => $"Dit is geen geldige waarde."));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseSession();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
